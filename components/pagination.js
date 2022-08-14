ed8acb8ba4ede57cfc6f7eb43102f9cf4531b2ea
kim-duyen-frontend <ntduyen255@gmail.com>
2022-08-14 16:56:36 +0700

    update css pagination

--- components/pagination.js
+++ components/pagination.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from "../styles/pagination.module.scss";
 
 const Pagination = ({ pg, setPage, isPreviousData }) => {
     return (
-        <div className={styles.pagination}>
-            <div className="container">
-                <div className={styles.container}>
-                    <Link href="#!">
-                        <a onClick={() => setPage(pg)} disabled={isPreviousData}>{pg}</a>
-                    </Link>
-                </div>
-            </div>
-        </div>
+        <>
+            <Link href="#!">
+                <a onClick={() => setPage(pg)} disabled={isPreviousData}>{pg}</a>
+            </Link>
+        </>
     );
 };
 
