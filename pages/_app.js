e66cc23256add99045dd7999ef4d8a92559bab22
kim-duyen-frontend <ntduyen255@gmail.com>
2022-08-16 10:24:15 +0700

    fix error delete react-query

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,9 @@
 import '../styles/main.scss';
 import 'react-toastify/dist/ReactToastify.css';
-import { QueryClient, QueryClientProvider, } from 'react-query';
-
-
-const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
-  return getLayout(<QueryClientProvider client={queryClient}><Component {...pageProps} /></QueryClientProvider>);
+  return getLayout(<Component {...pageProps} />);
 }
 
 export default MyApp
