6f9748a2e94942675e48524b7d4e8280a30b74c2
kim-duyen-frontend <ntduyen255@gmail.com>
2022-08-17 18:10:17 +0700

    get data api and css page detail every mentor

--- pages/mentors/[name].js
+++ pages/mentors/[name].js
@@ -1,10 +1,122 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getMentorDetail } from "../../utils/api/callAPI";
+import { FaHome, FaAngleRight, FaClock, FaCalendarCheck } from "react-icons/fa";
+import { IoLocationSharp } from "react-icons/io5";
+import Navbar from "../../components/navbar";
+import Footer from "../../components/footer";
+import Image from 'next/future/image';
+import styles from "../../styles/detailmentor.module.scss";
+import Link from 'next/link';
 
 const DetailMentor = ({ name }) => {
+    const [mentorDetail, setMentorDetail] = useState([]);
+    useEffect(() => {
+        getMentorDetail(name).then((json) => {
+            setMentorDetail(json.data)
+        })
+    }, [])
     return (
-        <div>
-            <h1>My example is {name}</h1>
-        </div>
+        <>
+            <Navbar />
+            <div className={styles.detailmentor}>
+                <div className="container">
+                    <div className={styles.container}>
+                        {mentorDetail && mentorDetail.length > 0 && mentorDetail.map((item) => (
+                            <div className={styles.content} key={item.id}>
+                                <div className={styles.banner}>
+                                    <div className={styles.text}>
+                                        <p>
+                                            <i><FaHome /></i>
+                                            <i><FaAngleRight /></i>
+                                            <span>Find Mentor <i><FaAngleRight /></i></span>
+                                            <span>{item.name}</span>
+                                        </p>
+                                    </div>
+                                    <div className={styles.imageUser}>
+                                        <Image className={styles.custom_image} priority src={item.thumbnail} alt={item.name} width={200} height={200} />
+                                    </div>
+                                </div>
+                                <div className={styles.grid}>
+                                    <div className={styles.infomation}>
+                                        <div className={styles.info}>
+                                            <h3>{item.name}</h3>
+                                            <p>{item.service} at {item.workplace}</p>
+                                            <p>{item.experience}</p>
+                                        </div>
+                                        <div className={styles.activity}>
+                                            <span>
+                                                <Link href="#!">
+                                                    <a>
+                                                        <IoLocationSharp />
+                                                        <span>{item.city}</span>
+                                                    </a>
+                                                </Link>
+                                            </span>
+                                            <span>
+                                                <Link href="#!">
+                                                    <a>
+                                                        <FaClock />
+                                                        <span>{item.activity}</span>
+                                                    </a>
+                                                </Link>
+                                            </span>
+                                            <span>
+                                                <Link href="#">
+                                                    <a>
+                                                        <FaCalendarCheck />
+                                                        <span>{item.message}</span>
+                                                    </a>
+                                                </Link>
+                                            </span>
+                                        </div>
+                                    </div>
+
+                                    <div className={styles.description}>
+                                        <h3>About</h3>
+                                        <p>
+                                            👋 Hi!!! I run the UX teams for Gmail @ Google, based out of San Francisco.
+                                            I also teach at UC Berkeley.
+                                            I have a strong track record of helping mentees land UX jobs, mostly in big tech.
+
+                                            As a mentee, you’ll have hands on support from me. Our first call entails going through a career worksheet. From there, I'll dive in with written notes and a structured plan. When you leave sessions, you'll know exactly what to do next and why.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className={styles.line}></div>
+
+                                <div className={styles.skills}>
+                                    <h3>Skills</h3>
+                                    <div className={styles.wrap}>
+                                        {item.skills.map((element, index) => (
+                                            <Link href="#!" key={index}>
+                                                <a>{element}</a>
+                                            </Link>
+                                        ))}
+                                    </div>
+                                </div>
+                                <div className={styles.line}></div>
+
+                                <div className={styles.topics}>
+                                    <h3>Topics</h3>
+                                    <div className={styles.wrap}>
+                                        {item.topics.map((element, index) => (
+                                            <Link href="#!" key={index}>
+                                                <a>{element}</a>
+                                            </Link>
+                                        ))}
+                                    </div>
+                                </div>
+                                <div className={styles.line}></div>
+                                <div className={styles.similar}>
+                                    <h3>Similar mentors</h3>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 };
 DetailMentor.getInitialProps = ({ query: { name } }) => {
--- utils/api/callAPI.js
+++ utils/api/callAPI.js
@@ -8,3 +8,9 @@ export const getMentorsPage = async (pageParam = 1) => {
     const response = await myAPI.get(`/mentors?page=${pageParam}`)
     return response.data
 }
+
+
+export const getMentorDetail = async (name) => {
+    const response = await myAPI.get(`/mentors/${name}`)
+    return response.data
+}
